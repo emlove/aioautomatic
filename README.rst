91a4c781863dbe709a5c0576772321895bce0e1f
Adam Mills <adam@armills.info>
2017-04-10 17:56:02 -0400

    Cookiecutter

---
+++ README.rst
@@ -0,0 +1,40 @@
+===============================
+aioautomatic
+===============================
+
+
+.. image:: https://img.shields.io/pypi/v/aioautomatic.svg
+        :target: https://pypi.python.org/pypi/aioautomatic
+
+.. image:: https://img.shields.io/travis/armills/aioautomatic.svg
+        :target: https://travis-ci.org/armills/aioautomatic
+
+.. image:: https://readthedocs.org/projects/aioautomatic/badge/?version=latest
+        :target: https://aioautomatic.readthedocs.io/en/latest/?badge=latest
+        :alt: Documentation Status
+
+.. image:: https://pyup.io/repos/github/armills/aioautomatic/shield.svg
+     :target: https://pyup.io/repos/github/armills/aioautomatic/
+     :alt: Updates
+
+
+Asyncio library for the Automatic API
+
+
+* Free software: Apache Software License 2.0
+* Documentation: https://aioautomatic.readthedocs.io.
+
+
+Features
+--------
+
+* TODO
+
+Credits
+---------
+
+This package was created with Cookiecutter_ and the `audreyr/cookiecutter-pypackage`_ project template.
+
+.. _Cookiecutter: https://github.com/audreyr/cookiecutter
+.. _`audreyr/cookiecutter-pypackage`: https://github.com/audreyr/cookiecutter-pypackage
+
---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+requirements = [
+    # TODO: put package requirements here
+    "aiohttp>=2.0.0",
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='aioautomatic',
+    version='0.1.0-dev',
+    description="Asyncio library for the Automatic API",
+    long_description=readme,
+    author="Adam Mills",
+    author_email='adam@armills.info',
+    url='https://github.com/armills/aioautomatic',
+    packages=[
+        'aioautomatic',
+    ],
+    package_dir={'aioautomatic':
+                 'aioautomatic'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="Apache Software License 2.0",
+    zip_safe=False,
+    keywords='aioautomatic',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
