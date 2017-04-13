13cba1599fec6039d057c53f66427f4788bda05a
Adam Mills <adam@armills.info>
2017-04-12 21:27:10 -0400

    Test forbidden scope

--- tests/test_client.py
+++ tests/test_client.py
@@ -43,3 +43,43 @@ def test_create_session_from_password(client):
         "scope": ("scope:location scope:vehicle:profile "
                   "scope:user:profile scope:trip scope:current_location"),
     }
+
+
+def test_scope_forbidden(client):
+    """Test opening a session from the users password."""
+    resp = AsyncMock()
+    resp.status = 200
+    resp.json.return_value = {
+        "access_token": "mock_access",
+        "expires_in": 123456,
+        "scope": ("scope:location scope:vehicle:profile "
+                  "scope:user:profile scope:trip"),
+        "refresh_token": "mock_refresh",
+        "token_type": "Bearer",
+    }
+
+    def get_resp(method, url, data, **kwargs):
+        if "scope:current_location" in data["scope"]:
+            resp.status = 403
+        else:
+            resp.status = 200
+        return resp
+
+    client._client_session.request.side_effect = get_resp
+
+    client.loop.run_until_complete(
+        client.create_session_from_password("mock_user", "mock_pass"))
+    assert client._client_session.request.called
+    assert len(client._client_session.request.mock_calls) == 2
+    assert client._client_session.request.mock_calls[0][1][0] == "POST"
+    assert client._client_session.request.mock_calls[0][1][1] == \
+        "https://accounts.automatic.com/oauth/access_token"
+    assert client._client_session.request.mock_calls[0][2]['data'] == {
+        "client_id": client.client_id,
+        "client_secret": client.client_secret,
+        "grant_type": "password",
+        "username": "mock_user",
+        "password": "mock_pass",
+        "scope": ("scope:location scope:vehicle:profile "
+                  "scope:user:profile scope:trip"),
+    }
