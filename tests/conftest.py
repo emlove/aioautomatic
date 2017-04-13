a3fca27470088452df937cecd24510e58a4a355b
Adam Mills <adam@armills.info>
2017-04-12 22:03:48 -0400

    Add test for session refresh

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,9 @@
 """Common fixtures for tests."""
 from aioautomatic.client import Client
+from aioautomatic.session import Session
 
 import pytest
-from tests.common import SessionMock
+from tests.common import AsyncMock, SessionMock
 
 
 @pytest.fixture
@@ -18,3 +19,20 @@ def client(aiohttp_session):
     """Create an aioautomatic client connected to a mock aiohttp session."""
     client = Client('mock_id', 'mock_secret', aiohttp_session)
     yield client
+
+
+@pytest.fixture
+def session(aiohttp_session):
+    """Create a test aioautomatic session."""
+    client = AsyncMock()
+    client.client_session = aiohttp_session
+    client.request_kwargs = {}
+    data = {
+        "access_token": "123",
+        "refresh_token": "ABCD",
+        "expires_in": 12345,
+        "scope": ("scope:location scope:vehicle:profile "
+                  "scope:user:profile scope:trip"),
+    }
+    session = Session(client, **data)
+    yield session
---
+++ tests/test_session.py
@@ -0,0 +1,39 @@
+"""Tests for automatic client."""
+from aioautomatic.client import Client
+
+from unittest.mock import MagicMock
+from tests.common import AsyncMock
+
+
+def test_session_refresh(session):
+    """Test refreshing a session with the refresh token."""
+    resp = AsyncMock()
+    resp.status = 200
+    resp.json.return_value = {
+        "access_token": "mock_access",
+        "expires_in": 123456,
+        "scope": ("scope:location scope:vehicle:profile "
+                  "scope:user:profile scope:trip"),
+        "refresh_token": "mock_refresh",
+        "token_type": "Bearer",
+    }
+    session._client_session.request.return_value = resp
+    refresh_token = session._refresh_token
+    renew_handle = MagicMock()
+    session._renew_handle = renew_handle
+    session._client.client_id="123"
+    session._client.client_secret="456"
+
+    session.loop.run_until_complete(session.refresh())
+    assert session._client_session.request.called
+    assert len(session._client_session.request.mock_calls) == 2
+    assert session._client_session.request.mock_calls[0][1][0] == "POST"
+    assert session._client_session.request.mock_calls[0][1][1] == \
+        "https://accounts.automatic.com/oauth/access_token"
+    assert session._client_session.request.mock_calls[0][2]['data'] == {
+        "client_id": "123",
+        "client_secret": "456",
+        "grant_type": "refresh_token",
+        "refresh_token": refresh_token,
+    }
+    assert renew_handle.cancel.called
